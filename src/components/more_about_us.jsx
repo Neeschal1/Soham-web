b560914e7459b8d484e843addec40fbc5f945a58
Neeschal1 <neeschalpok04@gmail.com>
2025-09-19 17:39:41 +0545

    completed more about us section

--- src/components/more_about_us.jsx
+++ src/components/more_about_us.jsx
@@ -1,12 +1,49 @@
 import React from "react";
+import Fonts from "../utils/fontsconfig";
 
 const Moreaboutus = () => {
   return (
     <>
-      <div className="items-center justify-center flex flex-1 flex-col bg-[#F2F1FF]">
-        <div className="flex flex-row items-center gap-3">
-            <h1 className="text-2xl">•</h1>
-        <h3> Soham</h3>
+      <div className="items-center justify-center flex flex-1 flex-col bg-[#F2F1FF] py-30">
+        <div className="flex flex-col gap-8 items-center justify-center">
+          <div className="flex gap-3 flex-col">
+            <div className="flex flex-row items-center gap-3 justify-center">
+              <h1 className="text-2xl font-poppins font-medium text-black/20">
+                •
+              </h1>
+              <h3
+                style={Fonts.poppins.regular}
+                className="font-poppins font-medium text-black/20"
+              >
+                {" "}
+                Soham
+              </h3>
+            </div>
+            <div>
+              <h1
+                style={Fonts.poppins.medium}
+                className="text-center text-4xl font-poppins font-medium"
+              >
+                Combining mindful practices to help users
+                <br /> stay calm, focused, and balanced—
+                <br />
+                anytime, anywhere.
+              </h1>
+              <h3
+                className="text-[#7D7D7D] text-center"
+                style={Fonts.poppins.regular}
+              >
+                We provide instant access to guided meditation and mindful
+                practices with tools for relaxation, stress
+                <br /> relief, and daily well-being
+              </h3>
+            </div>
+          </div>
+          <div>
+            <button className="bg-[#00DADA] py-4 px-10 rounded text-xl text-white hover:bg-black duration-800">
+              More about us
+            </button>
+          </div>
         </div>
       </div>
     </>
---
+++ src/utils/fontsconfig.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PoppinsLight from "../assets/fonts/Poppins-Light.ttf";
+import PoppinsRegular from "../assets/fonts/Poppins-Regular.ttf";
+import PoppinsMedium from "../assets/fonts/Poppins-Medium.ttf";
+import PoppinsSemiBold from "../assets/fonts/Poppins-SemiBold.ttf";
+import PoppinsBold from "../assets/fonts/Poppins-Bold.ttf";
+import KalamBold from "../assets/fonts/Kalam-Bold.ttf";
+
+const Fonts = {
+  poppins: {
+    light: { fontFamily: "Poppins, sans-serif", fontWeight: 300 },
+    regular: { fontFamily: "Poppins, sans-serif", fontWeight: 400 },
+    medium: { fontFamily: "Poppins, sans-serif", fontWeight: 500 },
+    semiBold: { fontFamily: "Poppins, sans-serif", fontWeight: 600 },
+    bold: { fontFamily: "Poppins, sans-serif", fontWeight: 700 },
+  }
+};
+
+export default Fonts;
