fe14373773935ddb9146b0f2a66c8f38d916d8f2
Neeschal1 <neeschalpok04@gmail.com>
2025-10-08 18:35:07 +0545

    journey screen is now responsive to desktop and mobile view

--- src/components/landing/journey.jsx
+++ src/components/landing/journey.jsx
@@ -76,20 +76,22 @@ const Journey = () => {
   return (
     <div className="w-full flex flex-col gap-10 p-5 md:p-10">
       {/* Desktop / Large Screens */}
-      <div className="hidden lg:flex flex-col gap-10">
+      <div className="hidden lg:flex flex-col gap-10 mt-20 mb-20">
         {/* Heading + Buttons */}
-        <div className="flex justify-between items-center w-full">
-          <div className="flex flex-col gap-4">
-            <h3
-              className="bg-[#F9F8FF] py-2 px-4 rounded-full w-fit"
-              style={Fonts.poppins.regular}
-            >
-              DISCOVER MORE
-            </h3>
-            <h1 style={Fonts.poppins.medium} className="text-4xl">
-              Start your Journey <br /> today
-            </h1>
-            <div className="flex gap-4 mt-2">
+        <div className="flex justify-center items-center w-full">
+          <div className="flex flex-1 flex-row gap-4 justify-center items-center">
+            <div className="flex flex-col gap-5">
+              <h3
+                className="bg-[#F9F8FF] py-2 px-4 rounded-full w-fit"
+                style={Fonts.poppins.regular}
+              >
+                DISCOVER MORE
+              </h3>
+              <h1 style={Fonts.poppins.medium} className="text-4xl">
+                Start your Journey <br /> today
+              </h1>
+            </div>
+            <div className="flex flex-1 gap-4  justify-end">
               {buttonoptions.map((items) => (
                 <button
                   style={Fonts.poppins.regular}
@@ -109,89 +111,91 @@ const Journey = () => {
         </div>
 
         {/* Detail Card Section */}
-        <div
-          ref={detailRef}
-          className={`w-full rounded-4xl flex flex-col p-6 items-center justify-between object-cover transition-all duration-700 ${
-            detailVisible
-              ? "opacity-100 translate-y-0"
-              : "opacity-0 translate-y-10"
-          }`}
-          style={{
-            backgroundImage: `url(${Detailcard})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="flex w-full flex-row items-center justify-between">
-            <button
-              className="bg-[#FFFFFF] py-3 px-4 rounded-full"
-              style={Fonts.poppins.regular}
-            >
-              Check full details
-            </button>
-            <div className="bg-white rounded-full py-3 px-3">
-              <LuArrowUpRight size={20} />
-            </div>
-          </div>
-          <div className="w-full flex flex-col gap-4 mt-4">
-            <h2
-              style={Fonts.poppins.regular}
-              className="text-white text-center text-2xl"
-            >
-              Together, let's journey towards the life of greater presence,
-              peace and well-being.
-            </h2>
-            <div className="w-full relative flex">
-              <input
-                className="w-full h-14 border border-white rounded-full p-5 pr-32 text-white bg-transparent placeholder-white"
-                type="email"
-                placeholder="Enter your email:"
-              />
+        <div className="lg:flex lg:flex-row lg:gap-10">
+          <div
+            ref={detailRef}
+            className={`w-9/12 h-150 rounded-4xl flex flex-col p-6 items-center justify-between object-cover transition-all duration-700 ${
+              detailVisible
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 translate-y-10"
+            }`}
+            style={{
+              backgroundImage: `url(${Detailcard})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <div className="flex w-full flex-row items-center justify-between">
               <button
+                className="bg-[#FFFFFF] py-3 px-4 rounded-full"
                 style={Fonts.poppins.regular}
-                className="absolute right-2 top-1/2 -translate-y-1/2 bg-[#00DADA] text-white px-5 py-2 rounded-full hover:bg-black transition duration-800"
               >
-                Get Started
+                Check full details
               </button>
-            </div>
-          </div>
-        </div>
-
-        {/* Journey Cards Section */}
-        <div
-          ref={cardsRef}
-          className={`flex flex-col gap-6 transition-all duration-700 ${
-            cardsVisible
-              ? "opacity-100 translate-y-0"
-              : "opacity-0 translate-y-10"
-          }`}
-        >
-          {journey_options.map((items) => (
-            <div
-              key={items.id}
-              className="h-auto w-full rounded-3xl bg-[#F2F2F2] p-4 flex flex-row gap-4"
-            >
-              <img
-                className="w-6/12 h-60 rounded-3xl object-cover"
-                src={items.image}
-              />
-              <div className="flex flex-col justify-between p-4 w-6/12 gap-3">
-                <h1 style={Fonts.poppins.medium} className="text-3xl">
-                  {items.title}
-                </h1>
-                <h3 style={Fonts.poppins.regular}>{items.subtitle}</h3>
-                <div className="flex gap-2 mt-2 items-center">
-                  <button
-                    style={Fonts.poppins.regular}
-                    className="bg-white py-2 px-3 rounded-full"
-                  >
-                    Discover more
-                  </button>
-                  <LuArrowUpRight size={18} />
-                </div>
+              <div className="bg-white rounded-full py-3 px-3">
+                <LuArrowUpRight size={20} />
               </div>
             </div>
-          ))}
+            <div className="w-full flex flex-col gap-4 mt-4">
+              <h2
+                style={Fonts.poppins.regular}
+                className="text-white text-center text-2xl"
+              >
+                Together, let's journey towards the life of greater presence,
+                peace and well-being.
+              </h2>
+              <div className="w-full relative flex">
+                <input
+                  className="w-full h-14 border border-white rounded-full p-5 pr-32 text-white bg-transparent placeholder-white"
+                  type="email"
+                  placeholder="Enter your email:"
+                />
+                <button
+                  style={Fonts.poppins.regular}
+                  className="absolute right-2 top-1/2 -translate-y-1/2 bg-[#00DADA] text-white px-5 py-2 rounded-full hover:bg-black transition duration-800"
+                >
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* Journey Cards Section */}
+          <div
+            ref={cardsRef}
+            className={`flex flex-col gap-6 transition-all duration-700 ${
+              cardsVisible
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 translate-y-10"
+            }`}
+          >
+            {journey_options.map((items) => (
+              <div
+                key={items.id}
+                className="h-72 w-full rounded-3xl bg-[#F2F2F2] p-4 flex flex-row gap-4"
+              >
+                <img
+                  className="w-6/12 h-64 rounded-3xl object-cover"
+                  src={items.image}
+                />
+                <div className="flex flex-col justify-between p-4 w-6/12 gap-3">
+                  <h1 style={Fonts.poppins.medium} className="text-3xl">
+                    {items.title}
+                  </h1>
+                  <h3 style={Fonts.poppins.regular}>{items.subtitle}</h3>
+                  <div className="flex gap-2 mt-2 items-center">
+                    <button
+                      style={Fonts.poppins.regular}
+                      className="bg-white py-2 px-3 rounded-full"
+                    >
+                      Discover more
+                    </button>
+                    <LuArrowUpRight size={18} />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
@@ -332,7 +336,7 @@ const Journey = () => {
                 style={Fonts.poppins.regular}
                 key={items}
                 onClick={() => setbtn(items)}
-                className={`text-sm font-poppins justify-between rounded-3xl px-8 py-3 transition-colors duration-800 ${
+                className={`text-sm font-poppins justify-between rounded-3xl px-6 py-3 transition-colors duration-800 ${
                   btn === items
                     ? "bg-[#000000] text-white"
                     : "border border-[#989898] text-black hover:bg-black hover:text-white"
